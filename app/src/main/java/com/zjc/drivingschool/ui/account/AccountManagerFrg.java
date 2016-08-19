e43bc0ee8affc81c121f857a955c5bf162584fdc
1020526698@qq.com <zjcx740811016>
2016-08-20 00:35:11 +0800

    1.初步完成我的钱包，包含金额获取和提交，以及支付宝，微信支付。

--- app/src/main/java/com/zjc/drivingschool/ui/account/AccountManagerFrg.java
+++ app/src/main/java/com/zjc/drivingschool/ui/account/AccountManagerFrg.java
@@ -1,8 +1,6 @@
 package com.zjc.drivingschool.ui.account;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,13 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.mobo.mobolibrary.logs.Logs;
 import com.mobo.mobolibrary.ui.base.ZBaseToolBarFragment;
-import com.mobo.mobolibrary.util.Util;
 import com.zjc.drivingschool.R;
 import com.zjc.drivingschool.api.ApiHttpClient;
 import com.zjc.drivingschool.api.ResultResponseHandler;
@@ -29,14 +25,8 @@ import com.zjc.drivingschool.eventbus.pay.PayAliAccountResultEvent;
 import com.zjc.drivingschool.eventbus.pay.PayWXAccountResultEvent;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import cn.beecloud.BCPay;
-import cn.beecloud.async.BCCallback;
-import cn.beecloud.async.BCResult;
-import cn.beecloud.entity.BCPayResult;
 import de.greenrobot.event.EventBus;
 
 /**
@@ -47,8 +37,10 @@ import de.greenrobot.event.EventBus;
  */
 public class AccountManagerFrg extends ZBaseToolBarFragment implements View.OnClickListener {
     private TextView tvAccount;
+    private EditText edtFee;
     private Button tvRecharge;
     private Button tvRechargeHistory;
+    private AccountBalance accountBalance;
 
     private RadioButton rb100;//数字代表充值的金额
     private RadioButton rb200;
@@ -61,7 +53,6 @@ public class AccountManagerFrg extends ZBaseToolBarFragment implements View.OnCl
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setHasOptionsMenu(true);
         EventBus.getDefault().register(this);
     }
 
@@ -106,6 +97,7 @@ public class AccountManagerFrg extends ZBaseToolBarFragment implements View.OnCl
         tvAccount = (TextView) rootView.findViewById(R.id.account_manager_frg_tv_total);
         tvRecharge = (Button) rootView.findViewById(R.id.account_frg_recharge);
         tvRechargeHistory = (Button) rootView.findViewById(R.id.account_frg_withdraw);
+        edtFee = (EditText) rootView.findViewById(R.id.account_manager_frg_edt_fee);
 
         rb100 = (RadioButton) rootView.findViewById(R.id.account_manager_frg_100);
         rb200 = (RadioButton) rootView.findViewById(R.id.account_manager_frg_200);
@@ -152,18 +144,26 @@ public class AccountManagerFrg extends ZBaseToolBarFragment implements View.OnCl
         }
     }
 
+    private int getSum() {
+        int sum = 0;
+        for (int i = 0; i < radioButtonList.size(); i++) {
+            if (radioButtonList.get(i).isChecked()) {
+                sum = Integer.parseInt(radioButtonList.get(i).getText().toString());
+            }
+        }
+        sum = sum + Integer.parseInt(edtFee.getEditableText().toString());
+        return sum;
+    }
+
     @Override
     public void onClick(View view) {
         int i = view.getId();
         if (i == R.id.account_frg_recharge) {
-            //充值界面
-//            AccountRechargeFragment fragment = new AccountRechargeFragment();
-//            replaceFrg(fragment, null);
-            toWXPay();
+            //支付类型选择界面
+            int sum = getSum();
+            AccountRechargeFragment fragment = AccountRechargeFragment.newInstance(sum);
+            replaceFrg(fragment, null);
         } else if (i == R.id.account_frg_withdraw) {
-            //提现界面
-//            AccountWithDrawFragment fragment = AccountWithDrawFragment.newInstance(Double.valueOf(tvAccount.getText().toString()));
-//            replaceFrg(fragment, null);
         }
     }
 
@@ -172,8 +172,8 @@ public class AccountManagerFrg extends ZBaseToolBarFragment implements View.OnCl
 
             @Override
             public void onResultSuccess(String result) {
-                AccountBalance balance = new AccountBalanceParser().parseResultMessage(result);
-                tvAccount.setText(balance.getBalance() + "");
+                accountBalance = new AccountBalanceParser().parseResultMessage(result);
+                tvAccount.setText(accountBalance.getBalance() + "");
             }
         });
     }
@@ -183,118 +183,9 @@ public class AccountManagerFrg extends ZBaseToolBarFragment implements View.OnCl
         getAccount();
     }
 
-    private void toWXPay() {
-        //对于微信支付, 手机内存太小会有OutOfResourcesException造成的卡顿, 以致无法完成支付
-        //这个是微信自身存在的问题
-        Map<String, String> mapOptional = new HashMap<>();
-
-        mapOptional.put("testkey1", "测试value值1");
-
-        if (BCPay.isWXAppInstalledAndSupported() && BCPay.isWXPaySupported()) {
-
-            BCPay.getInstance(getActivity()).reqWXPaymentAsync(
-                    "安卓微信支付测试",               //订单标题
-                    1,                           //订单金额(分)
-                    System.currentTimeMillis() + "",  //订单流水号
-                    mapOptional,            //扩展参数(可以null)
-                    bcCallback);            //支付完成后回调入口
-        } else {
-            Util.showCustomMsg("您尚未安装微信或者安装的微信版本不支持");
-        }
-    }
-
-    private String toastMsg;
-    //支付结果返回入口
-    BCCallback bcCallback = new BCCallback() {
-        @Override
-        public void done(final BCResult bcResult) {
-            final BCPayResult bcPayResult = (BCPayResult) bcResult;
-            //此处关闭loading界面
-//            loadingDialog.dismiss();
-
-            //根据你自己的需求处理支付结果
-            String result = bcPayResult.getResult();
-
-            /*
-              注意！
-              所有支付渠道建议以服务端的状态金额为准，此处返回的RESULT_SUCCESS仅仅代表手机端支付成功
-            */
-            Message msg = mHandler.obtainMessage();
-            //单纯的显示支付结果
-            msg.what = 2;
-            if (result.equals(BCPayResult.RESULT_SUCCESS)) {
-                toastMsg = "用户支付成功";
-            } else if (result.equals(BCPayResult.RESULT_CANCEL))
-                toastMsg = "用户取消支付";
-            else if (result.equals(BCPayResult.RESULT_FAIL)) {
-                toastMsg = "支付失败, 原因: " + bcPayResult.getErrCode() +
-                        " # " + bcPayResult.getErrMsg() +
-                        " # " + bcPayResult.getDetailInfo();
-
-                /**
-                 * 你发布的项目中不应该出现如下错误，此处由于支付宝政策原因，
-                 * 不再提供支付宝支付的测试功能，所以给出提示说明
-                 */
-                if (bcPayResult.getErrMsg().equals("PAY_FACTOR_NOT_SET") &&
-                        bcPayResult.getDetailInfo().startsWith("支付宝参数")) {
-                    toastMsg = "支付失败：由于支付宝政策原因，故不再提供支付宝支付的测试功能，给您带来的不便，敬请谅解";
-                }
-
-                /**
-                 * 以下是正常流程，请按需处理失败信息
-                 */
-                Logs.i(toastMsg);
-
-                if (bcPayResult.getErrMsg().equals(BCPayResult.FAIL_PLUGIN_NOT_INSTALLED)) {
-                    //银联需要重新安装控件
-                    msg.what = 1;
-                }
-
-            } else if (result.equals(BCPayResult.RESULT_UNKNOWN)) {
-                //可能出现在支付宝8000返回状态
-                toastMsg = "订单状态未知";
-            } else {
-                toastMsg = "invalid return";
-            }
-
-            mHandler.sendMessage(msg);
-
-
-            if (bcPayResult.getId() != null) {
-                //你可以把这个id存到你的订单中，下次直接通过这个id查询订单
-//                Log.w(TAG, "bill id retrieved : " + bcPayResult.getId());
-
-//                //根据ID查询，此处只是演示如何通过id查询订单，并非支付必要部分
-//                getBillInfoByID(bcPayResult.getId());
-            }
-
-        }
-    };
-
-
-    // Defines a Handler object that's attached to the UI thread.
-    // 通过Handler.Callback()可消除内存泄漏警告
-    private Handler mHandler = new Handler(new Handler.Callback() {
-        /**
-         * Callback interface you can use when instantiating a Handler to avoid
-         * having to implement your own subclass of Handler.
-         *
-         * handleMessage() defines the operations to perform when
-         * the Handler receives a new Message to process.
-         */
-        @Override
-        public boolean handleMessage(Message msg) {
-            switch (msg.what) {
-                case 2:
-                    Toast.makeText(getActivity(), toastMsg, Toast.LENGTH_SHORT).show();
-                    break;
-            }
-            return true;
-        }
-    });
 
     public void onEventMainThread(PayAliAccountResultEvent event) {
-
+        getAccount();
     }
 
     public void onEventMainThread(PayWXAccountResultEvent event) {
